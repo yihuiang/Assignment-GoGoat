a9dee5a7d6a9113b344d751610caedc764ad430f
Devion333 <165369655+Devion333@users.noreply.github.com>
2024-05-20 19:26:33 +0800

    Paneled the menu, dummy login page

---
+++ Manager asm/ManagerPages/Page_MenuCatalog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manager_asm.Pages
+{
+    public partial class Page_MenuCatalog : UserControl
+    {
+        public Page_MenuCatalog()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_MenuCatalog_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.Columns.Add("ID", 50);
+            listView1.Columns.Add("Name", 120);
+            listView1.Columns.Add("Price", 120);
+
+
+            SqlConnection conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\GoDB.mdf;Integrated Security=True;TrustServerCertificate=True;Initial Catalog=GoDB; Integrated Security = True;");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from MenuCatalog", conn);
+            SqlDataReader da;
+            da = cmd.ExecuteReader();
+            while (da.Read())
+            {
+                var item1 = listView1.Items.Add(da[0].ToString());
+                item1.SubItems.Add(da[1].ToString());
+                item1.SubItems.Add(da[2].ToString());
+            }
+            conn.Close();
+        }
+    }
+}
---
+++ Manager asm/frmCustomerUI.cs
@@ -0,0 +1,35 @@
+﻿using Manager_asm.User_Control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manager_asm
+{
+    public partial class frmCustomerUI : Form
+    {
+        public frmCustomerUI()
+        {
+            InitializeComponent();
+            Page_Menu page_Menu = new Page_Menu();
+            addpage(page_Menu);
+        }
+        private void addpage(UserControl Page)
+        {
+            Page.Dock = DockStyle.Fill;
+            panelCustomer.Controls.Clear();
+            panelCustomer.Controls.Add(Page);
+            Page.BringToFront();
+        }
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            Page_Menu page_Menu = new Page_Menu();
+            addpage(page_Menu);
+        }
+    }
+}

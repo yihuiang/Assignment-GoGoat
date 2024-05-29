9c821ac2ecd2c46f25c36ee05066b76c3b66c08d
Devion333 <165369655+Devion333@users.noreply.github.com>
2024-05-29 11:49:36 +0800

    database connection

---
+++ Manager asm/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_asm
+{
+    internal class Customer
+    {
+        private string CustomerID;
+        private string name;
+        private string email;
+        private string phonenum;
+        private string address;
+        static SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myCS"].ToString());
+
+        public Customer(string CustomerID, string name, string email, string phonenum, string address)
+        {
+            this.CustomerID = CustomerID;
+            this.name = name;
+            this.email = email;
+            this.phonenum = phonenum;
+            this.address = address;
+        }
+
+
+
+        public string SubmitFeedback(string food, string staff, string price, string portion, string menu, string comments)
+        {
+            //validation check
+            string status = "";
+            if (food == "" || staff == "" || price == "" || portion == "" || menu == "")
+                status = "Please select all rating options before submiting.";
+
+            con.Open();
+            string query = "INSERT INTO FeedbackMenu (CustomerID, FoodQuality, Staff, Price, PortionSize, MenuVariety, Comments) " +
+               "VALUES (@CustomerID, @FoodQuality, @Staff, @Price, @PortionSize, @MenuVariety, @Comments)";
+            using (SqlCommand command = new SqlCommand(query))
+            {
+                // Add parameters to the SQL command
+                // command.Parameters.AddWithValue("@CustomerID", this.CustomerID);
+                command.Parameters.AddWithValue("@FoodQuality", food);
+                command.Parameters.AddWithValue("@Staff", staff);
+                command.Parameters.AddWithValue("@Price", price);
+                command.Parameters.AddWithValue("@PortionSize", portion);
+                command.Parameters.AddWithValue("@MenuVariety", menu);
+                command.Parameters.AddWithValue("@Comments", comments);
+
+                int rowsAffected = command.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                  status = ("Feedback submitted successfully.");
+                }
+                else
+                {
+                    status = ("Failed to submit feedback.");
+                }
+                return status;
+            }
+        }
+    }
+}
--- Manager asm/CustomerPages/Page_Feedback_Reservation.cs
+++ Manager asm/CustomerPages/Page_Feedback_Reservation.cs
@@ -21,5 +21,10 @@ namespace Manager_asm.CustomerPages
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
